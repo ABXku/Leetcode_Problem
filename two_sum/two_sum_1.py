86b5a5c33300bc4f2fff9ea8e5cb97bd3a0ce07d
ABXku <wo.kunanon_st@tni.ac.th>
2023-09-25 16:33:34 +0700

    added two_sum folder and two_sum_1.py

---
+++ two_sum/two_sum_1.py
@@ -0,0 +1,11 @@
+"""
+    Runtime = 2489ms -> Beats 26.86% of users with Python3
+    Memory = 16.93MB Beats 98.08% of users with Python3
+"""
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        length = len(nums)
+        for i in range(length):
+            for j in range(i + 1, length):
+                if (nums[i] + nums[j]) == target:
+                    return [i, j]
