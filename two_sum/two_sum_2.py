09962c918ec96d76b9e5d39ce5a9bb8bd37d8168
ABXku <wo.kunanon_st@tni.ac.th>
2023-09-26 17:12:50 +0700

    added two_sum_2.py

---
+++ two_sum/two_sum_2.py
@@ -0,0 +1,12 @@
+"""
+    Runtime = 587ms Beats 38.18% of users with Python3
+    Memory = 17.40MB Beats 57.21%of users with Python3
+"""
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        index = {}
+        for i in range(len(nums)):
+            index[nums[i]] = i
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums and index[target - nums[i]] != i:
+                return [i, index[target - nums[i]]]
